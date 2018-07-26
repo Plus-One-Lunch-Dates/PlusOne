2b4032e28285f6fcdd346c1576979980cc7fe84d
Matthew Reid <matthew.reid.os1@gmail.com>
2018-07-26 12:32:33 -0500

    Defined user IP for server connection

---
+++ server/routes.js
@@ -0,0 +1,11 @@
+
+//TODO: Connect to server.js
+
+/*TODO: app.get('/', )
+
+// Get IP of Connected User
+const user = (req.connection.remoteAddress).slice(7);
+
+// Call Helper to send User IP to ipStack API and get Lat/Long
+
+*/
