dcd499b3293cd3d5e7d42cbaee2d071af1e3c6ea
aturek14@gmail.com <aturek14@gmail.com>
2018-07-26 12:57:40 -0500

    connected to mlab database and defined schema

---
+++ database/index.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+const tokens = require('./database-config2.js');
+
+const { USER_MONGODB, PROD_MONGODB } = tokens;
+
+mongoose.connect(`mongodb://${USER_MONGODB}:${PROD_MONGODB}@ds145951.mlab.com:45951/plus-one`);
+
+const db = mongoose.connection;
+
+db.on('error', () => {
+  console.log('mongoose connection error');
+});
+
+db.once('open', () => {
+  console.log('mongoose connected successfully');
+});
+
+const userSchema = mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  ipAddress: { type: String, required: false },
+  cravings: { type: String, required: false },
+  price: { type: String, required: false },
+  attire: { type: String, required: false },
+});
+
+userSchema.plugin(uniqueValidator);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+  User,
+};
