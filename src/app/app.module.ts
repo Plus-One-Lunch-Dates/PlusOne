5050e883e7e50052d4a45bca6a4fe51f1487e6bd
aturek14@gmail.com <aturek14@gmail.com>
2018-07-25 21:34:13 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ChatComponent } from './chat/chat.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    ChatComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
