1e235bc1b6a85e28a4cdea814707c3196dd0aa56
Matthew Reid <matthew.reid.os1@gmail.com>
2018-07-27 11:26:56 -0500

    Changed port for firewall

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const app = require('./server');
 
-const port = process.env.PORT || 3456;
+const port = process.env.PORT || 80;
 
 app.listen(port, () => console.log(`listening on port ${port}.`));
