56782bf15f10e96e06b9f7abee3154bd04249770
Senai Ayalew <senaiayalew@gmail.com>
2018-07-27 15:18:16 -0500

    Fixed merge conflict

--- server/index.js
+++ server/index.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-const app = require('./server');
-
-const port = process.env.PORT || 3456;
-
-app.listen(port, () => console.log(`listening on port ${port}.`));
-=======
 const app = require('./server');
 
 const port = process.env.PORT || 80;
 
 app.listen(port, () => console.log(`listening on port ${port}.`));
->>>>>>> 909386f65979aeead0f2d81197e054e2bac1eda6
--- server/server.js
+++ server/server.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-const express = require('express');
-const { urlencoded, json } = require('body-parser');
-const { join } = require('path');
-
-const app = express();
-
-app.use(urlencoded({ extended: false }));
-
-app.use(json());
-
-// Harvey's middleware
-app.use((req, res, next) => {
-  console.log(`${req.method}: ${req.path}`);
-  next();
-});
-
-app.use(express.static(join(__dirname, '../dist/mean-angular6')));
-
-
-module.exports = app;
-=======
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -41,4 +19,3 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, '../dist/mean-angular6')));
 
 module.exports = app;
->>>>>>> 909386f65979aeead0f2d81197e054e2bac1eda6
