cbf3fcf5232608f81d6e181e80b2a09563d9000b
aturek14@gmail.com <aturek14@gmail.com>
2018-07-27 16:39:18 -0500

    trying to establish routing in separate files

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,25 @@
+const dbhelpers = require('../database/database-helpers');
+const serverHelpers = require('./helpers');
 
-// TODO: Connect to server.js
+const saveUser = dbhelpers.saveUser;
+const updatePreferences = dbhelpers.updatePreferences;
+const matchMaker = dbhelpers.matchMaker;
+const addUserLocation = serverHelpers.addUserLocation;
+
+// app.post('/signup', cb)
+
+module.exports = (app) => {
+  app.post('/email', (req) => {
+    // Get IP of Connected User
+    const ip = (req.connection.remoteAddress).slice(7);
+    console.log(req, 'request');
+    console.log(ip, 'ip address');
+  });
+};
 
 /* TODO: app.get('/', )
 
-// Get IP of Connected User
-const user = (req.connection.remoteAddress).slice(7);
 
-// Call Helper to send User IP to ipStack API and get Lat/Long
+// Call Helper to send User IP to ipStack API and get location
 
 */
--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const db = require('../database/index')
-const dbhelpers = require('../database/database-helpers');
 
 const app = express();
+
+require('./routes')(app);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
