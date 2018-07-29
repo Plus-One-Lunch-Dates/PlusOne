8df25cc7e17c0332d96ab836969b11db67e636ec
Senai Ayalew <senaiayalew@gmail.com>
2018-07-28 21:20:27 -0500

    Home Component save button clicks to model object and send post request to '/home/:email' when Match me button is clicked

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,27 +29,17 @@ export class HomeComponent implements OnInit {
     // console.log(event.target.innerHTML.toString());
     this.model.email = 's.ayalew@yahoo.com'
     this.model.cravings = event.target.innerHTML.toString();
-    console.log(this.model.cravings);
-    console.log('cravings');
-    console.log(this.model.cravings);
   }
   price(event) {
     // console.log(event.target.innerHTML.toString());
     this.model.price = event.target.innerHTML.toString();
-    console.log(this.model.price);
-    console.log('priceee');
-    console.log(this.model.price);
   }
   attire(event) {
     // console.log(event.target.innerHTML.toString());
     this.model.attire = event.target.innerHTML.toString();
-    console.log(this.model.attire);
-    console.log('attire');
-    console.log(this.model.attire);
   }
 
   matchMe(model){
-    console.log(model, 'model')
     this.Http.gotoChat(model)
   }
 
