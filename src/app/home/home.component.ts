58fe854040affd913fdb0c57b6324e40bcbdfb4b
Senai Ayalew <senaiayalew@gmail.com>
2018-07-28 21:05:48 -0500

    added click evenets to every button in home.html created function in html.component to call http request to send a post request to /home/:email

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  model: any = {}
+
+  constructor(private Http: HttpService) { }
 
   ngOnInit() {
+  }
+
+  log(){
+    console.log(this.model);
+  }
+
+  signupUser(model) {
+    this.Http.home(model);
+  }
+
+  category(event) {
+    // console.log(event.target.innerHTML.toString());
+    this.model.email = 's.ayalew@yahoo.com'
+    this.model.cravings = event.target.innerHTML.toString();
+    console.log(this.model.cravings);
+    console.log('cravings');
+    console.log(this.model.cravings);
+  }
+  price(event) {
+    // console.log(event.target.innerHTML.toString());
+    this.model.price = event.target.innerHTML.toString();
+    console.log(this.model.price);
+    console.log('priceee');
+    console.log(this.model.price);
+  }
+  attire(event) {
+    // console.log(event.target.innerHTML.toString());
+    this.model.attire = event.target.innerHTML.toString();
+    console.log(this.model.attire);
+    console.log('attire');
+    console.log(this.model.attire);
+  }
+
+  matchMe(model){
+    console.log(model, 'model')
+    this.Http.gotoChat(model)
   }
 
 }
