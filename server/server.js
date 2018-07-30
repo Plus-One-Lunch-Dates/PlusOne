36c5e34e62e621712c3ebef3dc67612c954471e4
Matthew Reid <matthew.reid.os1@gmail.com>
2018-07-30 11:58:53 -0500

    Matching functional based on preferences and other users

--- server/server.js
+++ server/server.js
@@ -31,8 +31,8 @@ app.use(express.static(path.join(__dirname, '../dist/mean-angular6')));
 // Sign up for new account
 app.post('/signup', (req, res) => {
   // eslint-disable-next-line
-  const { firstName, lastName, username, email, password } = req.body;
-  saveUser(firstName, lastName, username, email, password);
+  const { firstName, lastName, username, number, email, password } = req.body;
+  saveUser(firstName, lastName, username, number, email, password);
   res.sendStatus(201);
 });
 
@@ -51,15 +51,16 @@ app.post('/login', (req, res) => {
     .catch(err => console.error(err, 'Error chceking user'));
 });
 
-// User submits preferences and looks for a match
+// User submits preferences andB looks for a match
 app.post('/home/:email', (req, res) => {
   // console.log(req.connection.remoteAddress, 'req.connection.remoteAddress');
   // console.log(req.connection, 'req.connection');
-  const { remoteAddress } = req.connection;
+  // const { remoteAddress } = req.connection;
   // eslint-disable-next-line
   const { email, cravings, price, attire } = req.body; 
   // TODO: Log to check req.body and IP
-  const userIP = remoteAddress.slice(7);
+  console.log(email, 'email');
+  const userIP = req.ip;
   console.log(userIP, 'userIP');
   updatePreferences(email, cravings, price, attire);
   getUserLocation(email, userIP)
@@ -67,11 +68,23 @@ app.post('/home/:email', (req, res) => {
     .then(promisedMatches => mapSeries(promisedMatches, promisedMatch => promisedMatch))
     .then((matches) => {
       if (matches.length < 1) {
+        console.log('No Matches Found');
         res.redirect('/home/:email');
       } else {
+        let match = 'No Matches Found';
+        for (let i = 0; i < matches.length; i += 1) {
+          if (matches[i].email !== email) {
+            match = { number: matches[i].number, name: matches[i].firstName };
+            console.log(match, ': match inside if');
+            return match;
+          }
+          console.log(match, ': match outside if');
+          return match;
+        }
+
         // TODO: Need to send the [0] from returned matches.
-        console.log(matches[0], 'matches[0] Match Found!');
-        res.redirect('/chat');
+        // console.log(matches[0], 'matches[0] Match Found!');
+        // res.send(matches[0].number);
       }
     })
     .catch(err => console.error(err));
